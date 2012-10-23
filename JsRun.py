8b08c1200ce3aa7ba89a65811f15f2f6742f74f1
Sindre Sorhus <sindresorhus@gmail.com>
2012-10-23 18:44:18 +0200

    init
    Thanks @paulirish for the idea!

---
+++ JsRun.py
@@ -0,0 +1,44 @@
+import sublime
+import sublime_plugin
+import urllib
+from subprocess import call
+
+
+settings = sublime.load_settings('JsRun.sublime-settings')
+
+
+class JsRunCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		selection = ''
+		for region in self.view.sel():
+			if region.empty():
+				selection += self.view.substr(self.view.line(region)) + '\n'
+			else:
+				selection += self.view.substr(region) + '\n'
+		selection = selection.strip()
+		if selection in ('', '\n'):
+			return
+		self.runjs(selection)
+
+	def _applescript(self, command):
+		call(['osascript', '-e', command])
+
+	def runjs(self, selection):
+		browser = settings.get('browser')
+		if browser == 'chrome':
+			self._applescript("""
+				tell application "Google Chrome"
+					execute front window's active tab javascript "%s"
+				end tell
+			""" % selection)
+		if browser == 'safari':
+			self._applescript("""
+				tell application "Safari" to do JavaScript "%s" in document 1
+			""" % selection)
+		if browser == 'opera':
+			self._applescript("""
+				tell application "Opera"
+					activate
+					set URL of front window to "javascript:%s"
+				end tell
+			""" % urllib.quote(selection))
---
+++ readme.md
@@ -0,0 +1,27 @@
+# JsRun
+
+Sublime plugin to run selected JavaScript code in the browser.
+
+
+## Install
+
+Install with [Package Control](http://wbond.net/sublime_packages/package_control)
+
+While the plugin is in Package Control queue you can get it by choosing `Package Control: Add repository` in the Command Palette *(Cmd+Shift+P)* and enter `https://raw.github.com/sindresorhus/sublime-jsrun/master/packages.json`. Then just install it normally through Package Control.
+
+
+## Getting started
+
+Set your browser in the settings: `chrome` (default), `safari`, `opera`.
+
+Select some text or just highlight a line and choose "Run JavaScript in the browser" in the Command Palette *(Cmd+Shift+P)*.
+
+You can also set up a keyboard shortcut to run the command by opening up `Preferences -> Key Bindings - User` and adding your shortcut with the command `js_run`.
+
+Currently supports Chrome, Safari and Opera on OS X. Firefox doesn't [expose JS to AppleScript](https://bugzilla.mozilla.org/show_bug.cgi?id=5704).
+
+
+## License
+
+[MIT License](http://en.wikipedia.org/wiki/MIT_License)
+(c) [Sindre Sorhus](http://sindresorhus.com)
